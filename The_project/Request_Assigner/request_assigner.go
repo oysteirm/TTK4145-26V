1b8e42c3bd3e3ba60ca7568737f010dbbed3b183
henninsu <henninsu@stud.ntnu.no>
2026-02-18 09:20:35 +0100

    Added input test comment to test hra

--- The_project/Request_Assigner/request_assigner.go
+++ The_project/Request_Assigner/request_assigner.go
@@ -5,3 +5,5 @@ import (
 	//"fmt"
 )
 
+//./tools/hall_request_assigner/hall_request_assigner -i '{"hallRequests":[[false,false],[true,false],[false,false],[false,true]],"states":{"one":{"behaviour":"moving","floor":2,"direction":"up","cabRequests":[false,false,false,true]},"two":{"behaviour":"idle","floor":0,"direction":"stop","cabRequests":[false,false,false,false]}}}'
+
