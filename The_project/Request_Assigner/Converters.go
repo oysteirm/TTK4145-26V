ee31d8910335b4c8a80ca5a55ed1124aeec1b372
henninsu <henninsu@stud.ntnu.no>
2026-02-25 13:03:08 +0100

    Added CC_To_Bool in Converters.go, and cleaned up a bit in Generating_RA_System_Data.go :)

--- The_project/Request_Assigner/Converters.go
+++ The_project/Request_Assigner/Converters.go
@@ -1,6 +1,14 @@
 package requestassigner
+//import something to use System_Data_t??
 
-CC_To_Bool(){
-
+func CC_To_Bool(CC Cyclic_Counter_t)bool{
+	if CC == CC_Uninit || CC == CC_No || CC = CC_Unconfirmed {
+		return false
+	}
+	if CC == CC_Confirmed || CC == CC_Done {
+		return true
+	}
+	else {
+		return nil
+	}
 }
-
--- The_project/Request_Assigner/Generating_RA_System_Data.go
+++ The_project/Request_Assigner/Generating_RA_System_Data.go
@@ -14,39 +14,40 @@ If this list == elevator_network_list then we send this data to the HSA
 -----------------------------------
 */
 
-//import something to use N_FLOORS, ex from elevator_io.go
+//import something to use System_Data_t
 
 import (
+	"../elevator"
 	"strconv"
 )
 
 func Generating_RA_System_Data(confirmed_system_data System_Data_t) RA_System_Data{
 	ra_system := RA_System_Data{}
-	ra_system.HallRequests = make([][N_HALL_CALLS]bool,N_FLOORS)
-	for i:=0; i < N_FLOORS;i++{
+	ra_system.HallRequests = make([][N_HALL_CALLS]bool,elevator.N_FLOORS)
+	for i:=0; i < elevator.N_FLOORS;i++{
 		for j:= 0; j < N_HALL_CALLS; j++{
-			ra_system.HallRequests[i][j] = Counter_To_Bool(confirmed_system_data.Hall_Request_Data[i][j])
+			ra_system.HallRequests[i][j] = CC_To_Bool(confirmed_system_data.Hall_Request_Data[i][j])
 		}
 	}
 
 	ra_system.States = make(map[string]RA_Local_Elevator_State)
-	for _,elevator := range confirmed_system_data.Elevator_Data{
+	for _,elev := range confirmed_system_data.Elevator_Data{
 
-		if !(elevator.Is_Alive.value) || !(elevator.Is_Able.value){
+		if !(elev.Is_Alive.value) || !(elev.Is_Able.value){
 			continue
 		}
 
-		cab_bools := make([]bool,N_FLOORS)
-		for i:= 0; i<N_FLOORS; i++{
-			cab_bools[i] = Counter_To_Bool(elevator.Cab_Requests[i])
+		cab_bools := make([]bool,elevator.N_FLOORS)
+		for i:= 0; i< elevator.N_FLOORS; i++{
+			cab_bools[i] = CC_To_Bool(elev.Cab_Requests[i])
 		}
 
-		Id_str := strconv.Itoa(elevator.Id)
+		Id_str := strconv.Itoa(elev.Id)
 
 		ra_system.States[Id_str] = RA_Local_Elevator_State{
-			Behavior:    elevator_behaviour_to_string(elevator.Elevator_Behaviour.value),
-			Floor:       elevator.Floor.value,
-			Direction:   elevator_dirn_to_string(elevator.Motor_Direction.value),
+			Behavior:    elevator.Elevator_behaviour_to_string(elev.Elevator_Behaviour.value),
+			Floor:       elev.Floor.value,
+			Direction:   elevator.Elevator_dirn_to_string(elev.Motor_Direction.value),
 			CabRequests: cab_bools,
 		}
 	}
