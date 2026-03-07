933026e7a317902e39a4ea733b998010a994ab22
oysteirm <165805814+oysteirm@users.noreply.github.com>
2026-03-07 11:05:45 +0100

    made main with todos on thoughts on how it can look

---
+++ theProject/main.go
@@ -0,0 +1,17 @@
+package theproject
+
+import(
+	//TODO: import what er need
+)
+
+func main(){
+	//TODO: make process pairs with primary-backup topology
+
+	//TODO: get the local ID from terminal command
+
+	//TODO: initialize all variables and constants
+	//TODO: initialize all the channels
+	//TODO: launch the go routines 
+
+	//TODO: forever loop?
+}
