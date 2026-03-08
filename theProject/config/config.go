8db488cee948ae88127e3b0c2bc36b28a7698a47
sanntid_pc <sanntid_pc@sanntid.com>
2026-03-08 13:09:02 +0100

    config again

--- theProject/config/config.go
+++ theProject/config/config.go
@@ -3,13 +3,16 @@ package config
 import "time"
 
 const (
+	LOCAL_ID int = 0 //0 or 1 or 2 
+
 	N_ELEVATORS int = 3
 	N_FLOORS int = 4
 	N_BUTTONS int = 3
 	N_UP_DOWN int = 2
 	DOOR_OPEN_DURATION time.Duration = 3 * time.Second
 	IS_FUNCTIONAL_TIMER_DURATION time.Duration = 9 * time.Second
-	const B_CAST_PORT int = 20014 //MAKE THE RIGHT
-	const PEER_UPDATE_PORT = 20015
+
+	B_CAST_PORT int = 20014
+	PEER_UPDATE_PORT int = 20015
 )
 
