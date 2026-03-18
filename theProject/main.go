1a4a3025150190b2a271c4f6c73e224586a50214
havan <havandarbandi@gmail.com>
2026-03-18 15:04:38 +0100

    Comment out processPairs

--- theProject/main.go
+++ theProject/main.go
@@ -9,11 +9,11 @@ import (
 	"theProject/elevator_IO"
 	"theProject/messageSync"
 	"theProject/networkDriver/peers"
-	"theProject/processPairs"
+	//"theProject/processPairs"
 )
 
 func main() {
-	processPairs.RunProcessPair()
+	//processPairs.RunProcessPair()
 
 	// Addresses from command inputs
 	localID := flag.Int("id", 0, "ID of this elevator (0, 1, 2, ...)")
